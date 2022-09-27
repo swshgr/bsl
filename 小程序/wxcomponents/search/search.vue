1235c5b0651daaf49781a88761f0c5802b225aff
swshgr <2598111890@qq.com>
2022-09-27 22:01:06 +0800

    '小程序'

---
+++ 小程序/wxcomponents/search/search.vue
@@ -0,0 +1,14 @@
+<template>
+	<view class="search-wrapper">
+		<van-search shape="round" background="#c00" placeholder="请输入搜索关键词" @focus="$emit('search')"  />
+	</view>
+</template>
+
+<script>
+export default {
+	
+}	
+</script>
+
+<style>
+</style>
